844c9c8c30ab3af7ea0dc7bd3493a6f33be06cdc
tosbysul <gitsutos@yahoo.com>
2022-10-02 23:15:35 +0530

    minor change in topBar/index.tsx

--- src/components/topBar/index.tsx
+++ src/components/topBar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TopBar = (props: {}) => {
+const TopBar = (props: any) => {
   return (
     <div className='flex min-h-10 max-h-24 h-20 flex-row bg-gradient-to-br items-center justify-between to-blue-600 from-blue-500' >
       <h1>CMS-TOS</h1>
